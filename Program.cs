c8d9e0ff56ee7f471119cf0cf16fb756b19bf2e0
csparra95 <48566625+csparra95@users.noreply.github.com>
2019-03-14 12:41:14 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Prueba
+    {
+        private static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance;
+
+            Console.WriteLine(s1.Metodo2());
+                Console.ReadKey();
+            
+
+        }
+    }
+}
